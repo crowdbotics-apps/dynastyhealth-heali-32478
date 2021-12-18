de7caebad5e6d4c0aa3d573d7bc87508fe74db86
Crowdbotics <team@crowdbotics.com>
2021-12-18 13:14:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "dynastyhealth_heali_32478",
+  url: "https://dynastyhealth_heali_32478.botics.co",
+  api: "https://dynastyhealth_heali_32478.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
